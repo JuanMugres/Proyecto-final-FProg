599aeedcbbcbf42c50e6f615c37ca8fb84cf9d63
JuanMugres <jmugres123@gmail.com>
2022-11-18 14:05:01 -0600

    añadiendo caja de texto para nombre y numero de asiento

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+function main() {
+	let tabla = document.createElement("table")
+	let fila = document.createElement("tr")
+	let celda = document.createElement("td")
+
+	
+
+	for (var i = 4; i <; i++) {
+		t
+	}
+
+
+}
+
