81dcd027efffc7626760933ae9ea28b655667204
JuanMugres <jmugres123@gmail.com>
2022-11-18 20:09:34 -0600

    enviando cambios menores sin importancia

--- js/main.js
+++ js/main.js
@@ -5,12 +5,19 @@ function main() {
 	let fila = document.createElement("tr")
 	let celda = document.createElement("td")
 
-	
-
-	for (var i = 4; i <; i++) {
-		t
+	for(i = 0; i < 10; i++){
+		fila.appendChild(celda)
 	}
 
 
+	for (var i = 0; i < 4; i++) {
+		tabla.appendChild(fila)
+	}
+	tabla.setAttribute("border", 2)
+	document.getElementById("tabla").appendChild(tabla)
+
+
 }
+
+main()
 
