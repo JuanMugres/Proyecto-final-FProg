1ccac4d0249ed27806719059c9923583a1db8651
JuanMugres <jmugres123@gmail.com>
2022-11-28 19:13:26 -0600

    Se añadió funcion de seleccion de destino al mapa

--- js/Mapa.js
+++ js/Mapa.js
@@ -1,2 +1,23 @@
-document.getElementsByName("Durango").setAttribute("fill", "red") //Debería agregar los atributos fill=red al elemento de durango, marcando de rojo al estado, pero no lo hace.
+
+function seleccionarDestino(id){
+    document.querySelectorAll('path')
+    .forEach((function(x){ x.setAttribute("style","fill: black");}))
+    document.getElementById("nombre").value = id
+    document.getElementById(id).setAttribute("style", "fill: red")
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+//Debería agregar los atributos fill=red al elemento de durango, marcando de rojo al estado, pero no lo hace.
 // fill="red" es un atributo que llena de color al elemento
